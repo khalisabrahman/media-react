397d62923129746290ee3881151de268ac3c9eba
khalisabrahman <khalisabrahman@gmail.com>
2023-06-12 14:55:05 +0800

    redux toolkit setup

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,22 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
 import { usersReducer } from "./slices/usersSlice";
+import { albumsApi } from "./apis/albumsApi";
 
 //The configureStore here replaces the original createStore from Redux. Unlike createStore, configureStore from Redux Toolkit not only creates a store but can also accept reducer functions as arguments and automatically sets up the Redux DevTools Extension for easy debugging.
 export const store = configureStore({
   reducer: {
     users: usersReducer,
+    [albumsApi.reducerPath]: albumsApi.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware().concat(albumsApi.middleware);
   },
 });
+
+setupListeners(store.dispatch);
 
 export * from "./thunks/fetchUsers";
 export * from "./thunks/addUser";
 export * from "./thunks/removeUser";
+export { useFetchAlbumsQuery } from "./apis/albumsApi";
