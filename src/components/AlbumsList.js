39e2d9bd84853f8335dbcff392b0ca649012c70c
khalisabrahman <khalisabrahman@gmail.com>
2023-06-16 22:07:43 +0800

    removeAlbum api

--- src/components/AlbumsList.js
+++ src/components/AlbumsList.js
@@ -2,9 +2,10 @@ import { useFetchAlbumsQuery, useAddAlbumMutation } from "../store";
 import Skeleton from "./Skeleton";
 import ExpandablePanel from "./ExpandablePanel";
 import Button from "./Button";
+import AlbumsListItem from "./AlbumsListItem";
 
 function AlbumsList({ user }) {
-  const { data, error, isLoading } = useFetchAlbumsQuery(user);
+  const { data, error, isFetching } = useFetchAlbumsQuery(user);
   const [addAlbum, results] = useAddAlbumMutation();
 
   const handleAddAlbum = () => {
@@ -12,25 +13,24 @@ function AlbumsList({ user }) {
   };
 
   let content;
-  if (isLoading) {
-    content = <Skeleton times={3} />;
+  if (isFetching) {
+    content = <Skeleton className="h-10 w-full" times={3} />;
   } else if (error) {
     content = <div>Error loading albums.</div>;
   } else {
     content = data.map((album) => {
-      const header = <div>{album.title}</div>;
-      return (
-        <ExpandablePanel key={album.id} header={header}>
-          List of photos in the album
-        </ExpandablePanel>
-      );
+      return <AlbumsListItem key={album.id} album={album} />;
     });
   }
 
   return (
     <div>
-      <div>Albums for {user.name} </div>
-      <Button onClick={handleAddAlbum}>+ Add Album</Button>
+      <div className="m-2 flex flex-row items-center justify-between">
+        <h3 className="text-lg font-bold">Albums for {user.name} </h3>
+      </div>
+      <Button loading={results.isLoading} onClick={handleAddAlbum}>
+        + Add Album
+      </Button>
       <div>{content}</div>
     </div>
   );
